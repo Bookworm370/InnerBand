37156b277069a368cc5fd3f118347ed1da262d30
John Blanco <john.blanco@effectiveui.com>
2010-10-01 09:51:56 -0600

    Added meta-include for easier include. (I recommend putting this in PCH)

---
+++ iBoost/iBoostIncludes.h
@@ -0,0 +1,35 @@
+//
+//  iBoostIncludes.h
+//  iBoost
+//
+//  Created by John Blanco on 10/1/10.
+//  Copyright 2010 Effective UI. All rights reserved.
+//
+
+#import "Macros.h"
+#import "Constants.h"
+
+#import "UIColor+Boost.h"
+#import "CGContext+Boost.h"
+#import "NSArray+Boost.h"
+#import "NSMutableArray+Boost.h"
+#import "NSObject+Boost.h"
+#import "NSString+Boost.h"
+#import "UIImage+Boost.h"
+#import "UIView+Boost.h"
+#import "NSNumber+Boost.h"
+#import "NSDate+Boost.h"
+
+#import "ViewUtil.h"
+#import "CoreDataStore.h"
+
+#import "MessageCenter.h"
+#import "DispatchMessage.h"
+#import "SequencedMessage.h"
+#import "HTTPGetRequestMessage.h"
+
+#import "IBHTMLLabel.h"
+#import "IBCoreTextLabel.h"
+#import "IBImageView.h"
+#import "IBHTableView.h"
+#import "IBButton.h"

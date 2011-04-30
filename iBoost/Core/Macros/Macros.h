e2f1284f31254236631b7e3d0ed99a8e3bd087a6
John Blanco <zablanc@gmail.com>
2011-04-30 10:16:58 -0600

    Replicated memory macros to new names.

--- iBoost/Core/Macros/Macros.h
+++ iBoost/Core/Macros/Macros.h
@@ -41,6 +41,11 @@
 
 // MEMORY
 
+// new versions
+#define SAFE_RELEASE(obj) ([obj release], obj = nil)
+#define SAFE_ASSIGN(obj, expr) ([(expr) retain], [obj release], obj = (expr))
+
+// old versions
 #define RELEASE_AND_NIL(obj) ([obj release], obj = nil)
 #define RETAIN_RELEASE_AND_ASSIGN_TO(obj, expr) ([(expr) retain], [obj release], obj = (expr))
 
